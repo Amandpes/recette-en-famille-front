8c66b06ac727573a6497c584ab146047c838bb47
P_Easy_A <70567979+P-Easy-A@users.noreply.github.com>
2023-02-02 15:44:09 +0100

    feat: add screen add recipe, add recipe css

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Homescreen from "./Screens/Homescreen";
 import Singinscreen from "./Screens/Singinscreen";
+import AddRecipeScreen from "./Screens/AddRecipeScreen";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import "./App.css";
 import {SigninScreen} from "./Screens/SignupScreen";
@@ -15,6 +16,7 @@ function App() {
         <Route path="/" element={<Homescreen />} />
         <Route path="/login" element={<Singinscreen />} />
         <Route path="/signup" element={<SigninScreen />} />
+        <Route path="/add-recipe" element={<AddRecipeScreen />} />
       </Routes>
     </BrowserRouter>
   );
